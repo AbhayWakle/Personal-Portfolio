43832dbd09600faeae8fb076ed95fe502cc51f04
Abhay Wakle <140650463+AbhayWakle@users.noreply.github.com>
2024-09-14 17:14:21 +0530

    navigation improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,26 @@ import { FaInstagram } from "react-icons/fa";
 import { TbBrandLeetcode } from "react-icons/tb";
 import { SiCodechef } from "react-icons/si";
 import logo from "../assets/logo1.png";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+
+    const navigate = useNavigate();
+
+    const homePage = () => {
+        navigate('/');
+    }
+
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-20" src={logo} alt="logo" />
+                <img
+                    className="mx-2 w-20 cursor-pointer"
+                    src={logo}
+                    alt="logo"
+                    onClick={homePage}
+                />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <a href="https://www.linkedin.com/in/abhay-wakle-559a96228/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
